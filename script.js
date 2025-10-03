ec87c39a67c0f22ba9d21cd9359214f022a93642
Rangoognar <morfin.alex09@gmail.com>
2025-10-03 12:22:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+@font-face {
+  font-family: 'SairaExtraCondensed';
+  src: url(SairaExtraCondensed-VariableFont_wght.ttf) format('truetype');
+}
+h1, p {
+    font-family: 'SairaExtraCondensed';
+}
